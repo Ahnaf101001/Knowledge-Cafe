abe122b589303bde6add1b67de054fb54ae86e53
Ahnaf Khan <ahnaf10100@gmail.com>
2024-03-15 03:22:39 +0600

    done with the bookmarks side

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,11 @@ function App() {
     setBookmarks(newBookmarks);
   }
 
-  const handleMarkAsRead = time =>{
+  const handleMarkAsRead = (id, time) =>{
     const newReadingTime = readingTime + time;
     setReadingTime(newReadingTime);
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
+    setBookmarks(remainingBookmarks);
   }
 
   return (
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import 'boxicons'
 
 const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
-    const {title, cover, author, author_img, reading_time, posted_date, hashtags} = blog;
+    const {id, title, cover, author, author_img, reading_time, posted_date, hashtags} = blog;
     return (
         <div className='mb-[40px]'>
             <img className="w-[845px]" src={cover} alt={`Cover Picture of the title: ${title}`} />
@@ -26,7 +26,7 @@ const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
                 }
             </p>
             <button 
-                onClick={() => handleMarkAsRead(reading_time)}
+                onClick={() => handleMarkAsRead(id, reading_time)}
                 className='mb-[32px]'
             >Mark As Read</button>
             <hr className='mb-[40px]'/>
